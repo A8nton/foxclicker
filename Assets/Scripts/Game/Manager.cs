5c0fbf9b84299314afe0bd268d29233f0ab27b9f
A8nton <toskagg01@gmail.com>
2020-08-10 10:04:42 +0300

    init commit

---
+++ Assets/Scripts/Game/Manager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour {
+
+    [SerializeField]
+    private Text _score;
+
+    public int coinUpdate = 1;
+    public int coins;
+
+    public void Start() {
+        int savedCoins = PlayerPrefs.GetInt("coins");
+        coins = savedCoins;
+        _score.text = "FOXCOINS: " + savedCoins.ToString();
+    }
+
+    public void PlusCoins() {
+        if (coins >= 10) {
+            coins -= 10;
+            coinUpdate++;
+        }
+    }
+
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            coins += coinUpdate;
+            PlayerPrefs.SetInt("coins", coins);
+            PlayerPrefs.SetInt("coinUpdate", coinUpdate);
+            _score.text = "FOXCOINS: " + coins;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _uiMenu;
+    [SerializeField]
+    private GameObject _menu;
+
+    public bool _isMenuActive;
+
+    public void OnClick(string game) {
+        if (_isMenuActive == false) {
+            _isMenuActive = true;
+            _menu.SetActive(true);
+            _uiMenu.SetActive(true);
+        }
+        else if (_isMenuActive == true) {
+            _isMenuActive = false;
+            _menu.SetActive(false);
+            _uiMenu.SetActive(false);
+        }
+
+    }
+}
